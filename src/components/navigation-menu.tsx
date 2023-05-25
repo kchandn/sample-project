935e6e85c54874eb2e98e8c55e9ceb6b892e45bf
Chandan.kumar <chandan.kumar@trivium-esolutions.com>
2023-05-25 17:05:38 +0530

    Bootstrap package installed and list styles changed.

--- src/components/navigation-menu.tsx
+++ src/components/navigation-menu.tsx
@@ -1,11 +1,11 @@
 function NavigationMenu() {
   return (
-    <ul>
-      <li>Home</li>
-      <li>Login</li>
-      <li>Sign Up</li>
-      <li>Contact Us</li>
-      <li>About Us</li>
+    <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
     </ul>
   );
 }
