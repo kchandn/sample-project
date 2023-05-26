d88a8d5c7e9b3a8dd4464e526620e66885109fb2
Chandan.kumar <chandan.kumar@trivium-esolutions.com>
2023-05-26 11:45:05 +0530

    Added menu in navbar.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,12 @@
 function Header() {
+  const menuItem = [
+    "Home",
+    "Services",
+    "Technologies",
+    "Careers",
+    "About Us",
+    "Contact Us",
+  ];
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -19,48 +27,21 @@ function Header() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
+              {menuItem.map((menu, index) => (
+                <li
+                  className="nav-item"
+                  key={menu}
+                  onClick={() => console.log(index)}
                 >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  {/* <li><a className="dropdown-divider"></li> */}
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
+                  <a
+                    className="nav-link {manu === 'Home' ? active : ''}"
+                    aria-current="page"
+                    href="#"
+                  >
+                    {menu}
+                  </a>
+                </li>
+              ))}
             </ul>
             <form className="d-flex" role="search">
               <input
